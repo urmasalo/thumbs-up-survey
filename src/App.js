dbe5db8c78fc1594739f97c633c8985db5d1191e
Mauricio Salazar <mauriciosalazar@Mauricios-MacBook-Pro.local>
2021-01-31 15:54:33 -0500

    submit banner and button  added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,3 @@
-import '../src/styles/_header.scss'
-import '../src/styles/_heroCard.scss'
-import '../src/styles/_alert.scss'
-import '../src/styles/_mainStage.scss'
-
-
 
 function App() {
   return (
