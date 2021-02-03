b34b03f200f653cdbea7b0f50577075de1e71cc7
Mauricio Salazar <mauriciosalazar@Mauricios-MacBook-Pro.local>
2021-02-03 06:49:29 -0500

    behavior has voted or not added

--- src/components/Candidate.jsx
+++ src/components/Candidate.jsx
@@ -6,7 +6,10 @@ const Candidate = ({ image, name, description, last_update, categories }) => {
   const [dislikes, setDislikes] = useState(0);
   const [vote, setVote] = useState("");
   const popularity = calculatePopularity(likes, dislikes);
-  const unpopularity = popularity > 0 ? 100 - popularity : 0;
+  const unpopularity =
+    popularity > 0 ? 100 - popularity : dislikes > 0 ? 100 : 0;
+  const isPopular = popularity >= unpopularity ? true : false;
+  const [hasVoted, setHasVoted] = useState(false);
 
   const changeHandler = (event) => {
     setVote(event.target.value);
@@ -15,23 +18,13 @@ const Candidate = ({ image, name, description, last_update, categories }) => {
   const submitVote = (event) => {
     event.preventDefault();
 
-    if (unpopularity > popularity) {
-      let statusIconContainer = document.getElementById(
-        "status-icon-container"
-      );
-      statusIconContainer.className += " unpopular-container";
-    }
-    // else if (unpopularity < popularity) {
-    //   let statusIconContainer = document.getElementById(
-    //     "status-icon-container"
-    //   );
-    //   statusIconContainer.className += "";
-    // }
-
-    if (vote === "yes") {
-      setLikes((prevLikes) => prevLikes + 1);
-    } else if (vote === "no") {
-      setDislikes((prevDislikes) => prevDislikes + 1);
+    if (vote === "yes" || vote === "no") {
+      if (vote === "yes") {
+        setLikes((prevLikes) => prevLikes + 1);
+      } else if (vote === "no") {
+        setDislikes((prevDislikes) => prevDislikes + 1);
+      }
+      setHasVoted(true);
     }
   };
 
@@ -39,7 +32,11 @@ const Candidate = ({ image, name, description, last_update, categories }) => {
     <div className="candidates__card">
       <img src={image} alt={name} className="candidates__card__image" />
       <div className="candidates__card__title__wrapper">
-        <span id="status-icon-container" className="candidates__thumbs-status">
+        <span
+          className={`candidates__thumbs-status candidates__thumbs-status--${
+            isPopular ? "up" : "down"
+          }`}
+        >
           <i className="far fa-thumbs-up"></i>
         </span>
         <h2 className="candidates__card__title">{name}</h2>
@@ -51,48 +48,59 @@ const Candidate = ({ image, name, description, last_update, categories }) => {
           in {categories}
         </span>
       </h4>
-      <h3 className="candidates__card__description">{description}</h3>
+      <h3 className="candidates__card__description">
+        {hasVoted ? "Thanks you for voting!" : description}
+      </h3>
       <div className="candidates__card__divisor">
-        <form className="candidates__card__form" onSubmit={submitVote}>
-          <div
-            onChange={changeHandler}
-            className="candidates__card__form__vote"
-          >
-            <input
-              value="yes"
-              id={`thumbs-up-${name}`}
-              type="radio"
-              name={`vote-${name}`}
-              className="hide candidates__card__form__radio"
-            ></input>
-            <label
-              htmlFor={`thumbs-up-${name}`}
-              className="candidates__thumbs-indicator candidates__thumbs-indicator__up"
-            >
-              <i className="far fa-thumbs-up"></i>
-            </label>
-
-            <input
-              value="no"
-              id={`thumbs-down-${name}`}
-              type="radio"
-              name={`vote-${name}`}
-              className="hide candidates__card__form__radio"
-            ></input>
-            <label
-              htmlFor={`thumbs-down-${name}`}
-              className="candidates__thumbs-indicator candidates__thumbs-indicator__down"
-            >
-              <i className="far fa-thumbs-up inverted"></i>
-            </label>
-          </div>
+        {hasVoted ? (
           <button
-            type="submit"
+            onClick={() => setHasVoted(false)}
             className="candidates__card__view-report-button"
           >
-            Vote now
+            Vote Again
           </button>
-        </form>
+        ) : (
+          <form className="candidates__card__form" onSubmit={submitVote}>
+            <div
+              onChange={changeHandler}
+              className="candidates__card__form__vote"
+            >
+              <input
+                value="yes"
+                id={`thumbs-up-${name}`}
+                type="radio"
+                name={`vote-${name}`}
+                className="hide candidates__card__form__radio"
+              ></input>
+              <label
+                htmlFor={`thumbs-up-${name}`}
+                className="candidates__thumbs-indicator candidates__thumbs-indicator__up"
+              >
+                <i className="far fa-thumbs-up"></i>
+              </label>
+
+              <input
+                value="no"
+                id={`thumbs-down-${name}`}
+                type="radio"
+                name={`vote-${name}`}
+                className="hide candidates__card__form__radio"
+              ></input>
+              <label
+                htmlFor={`thumbs-down-${name}`}
+                className="candidates__thumbs-indicator candidates__thumbs-indicator__down"
+              >
+                <i className="far fa-thumbs-up inverted"></i>
+              </label>
+            </div>
+            <button
+              type="submit"
+              className="candidates__card__view-report-button"
+            >
+              Vote now
+            </button>
+          </form>
+        )}
       </div>
       <div className="candidates__graphic-report">
         <div className="candidates__graphic-report__bars">
@@ -106,7 +114,10 @@ const Candidate = ({ image, name, description, last_update, categories }) => {
           ></div>
         </div>
         {/* stats bar */}
-        <div className="candidates__graphic-report__values">
+        <div
+          className="candidates__graphic-report__values"
+          style={{ display: likes === 0 && dislikes === 0 ? "none" : "" }}
+        >
           <div className="">
             <span className="candidates__graphic-report__thumbs-up__icon">
               <i className="far fa-thumbs-up"> </i>
